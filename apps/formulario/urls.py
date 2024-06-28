a3e82d4136705ca4ceb33dce689b08ba1a4d6206
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-06-28 08:38:40 -0400

    feat: endpoint reporte y migracion token blacklist

--- apps/formulario/urls.py
+++ apps/formulario/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from apps.formulario.views import FormularioController
+from .views import FormularioController, GeneralReporteCompleto
 
 router = DefaultRouter()
 app_name = "formulario"
 router.register(r"formulario", FormularioController, basename="formulario")
 urlpatterns = router.urls
+urlpatterns += [
+    path("formulario/reportecompleto/<int:id>/", GeneralReporteCompleto.as_view()),
+]
--- apps/formulario/views.py
+++ apps/formulario/views.py
@@ -1,12 +1,20 @@
+import traceback
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 from apps.formulario.serializers import FormularioSerializer
 from config.utils.utlis_view_api import CustomPagination
+
+from ..users.authentication import IsTokenValid
+from .models import Formulario
+from .utils.reportes import crear_reporte_completo_paciente
 
 # Create your views here.
 
@@ -19,7 +27,7 @@ class FormularioController(viewsets.ModelViewSet):
         SearchFilter,
         OrderingFilter,
     ]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsTokenValid]
 
     filterset_fields = {
         "id": ["exact"],
@@ -36,3 +44,30 @@ class FormularioController(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.serializer_class.Meta.model.objects.all()
+
+
+class GeneralReporteCompleto(APIView):
+    permission_classes = [IsAuthenticated, IsTokenValid]
+
+    def get(self, request, id):
+        try:
+            q = Formulario.objects.filter(id=id)
+            if not q.exists():
+                return JsonResponse(
+                    {
+                        "status": "error",
+                        "message": "Error de en servidor",
+                    },
+                    status=404,
+                )
+            return crear_reporte_completo_paciente(q)
+
+        except:
+            print(traceback.format_exc())
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": "Error de en servidor",
+                },
+                status=500,
+            )
