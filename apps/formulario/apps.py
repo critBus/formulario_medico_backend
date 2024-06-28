ea141f5bd7eb360d27e428ec1d4443cb4c287bc9
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-06-28 10:07:11 -0400

    feat: roles por defecto

--- apps/formulario/apps.py
+++ apps/formulario/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def config_app(sender, **kwargs):
+    from .utils.utils_roles_por_defecto import crear_roles_django_default
+
+    crear_roles_django_default()
 
 
 class FormularioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.formulario"
+
+    def ready(self):
+        post_migrate.connect(config_app, sender=self)
